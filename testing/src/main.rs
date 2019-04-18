ea79eb0b14eea7fd9d00a3ca038bbb2137960d3f
Prometeo <xairi.valdivia@gmail.com>
2019-04-18 14:52:16 -0500

    Testing if the file exists

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,5 +1,8 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
 fn main() {
-    println!("hello World!")
+    // println!("hello World!")
 }
 
 fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
@@ -20,3 +23,14 @@ fn find_a_match() {
 // Note: We could also make this function return a String, but that would change its behavior.
 // Instead of writing to the terminal directly, it would then collect everything into a string,
 // and dump all the results in one go at the end.
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<std::error::Error>> {
+    let mut cmd = Command::main_binary()?;
+    cmd.arg("foobar").arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("No such file or directory"));
+
+    Ok(())
+}
