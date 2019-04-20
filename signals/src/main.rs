5f9bd1372c1b7d69f69c17db1ade8fd9ec25c8df
Prometeo <xairi.valdivia@gmail.com>
2019-04-19 22:09:32 -0500

    Signals with channels

--- signals/src/main.rs
+++ signals/src/main.rs
@@ -1,5 +1,16 @@
+use crossbeam_channel::{bounded, select, tick, Receiver};
 use signal_hook::{iterator::Signals, SIGINT};
+use std::time::Duration;
 use std::{error::Error, thread};
+
+fn ctrl_channel() -> Result<Receiver<()>, ctrlc::Error> {
+    let (sender, receiver) = bounded(100);
+    ctrlc::set_handler(move || {
+        let _ = sender.send(());
+    })?;
+
+    Ok(receiver)
+}
 
 fn main() -> Result<(), Box<Error>> {
     // getting only ctrl+c signal
@@ -16,6 +27,27 @@ fn main() -> Result<(), Box<Error>> {
             println!("Received signal {:?}", sig);
         }
     });
+
+    Ok(())
+}
+
+// using channels
+fn main2() -> Result<(), exitfailure::ExitFailure> {
+    let ctrl_c_events = ctrl_channel()?;
+    let ticks = tick(Duration::from_secs(1));
+
+    loop {
+        select! {
+            recv(ticks) -> _ => {
+                println!("working!");
+            }
+            recv(ctrl_c_events) -> _ => {
+                println!();
+                println!("Goodbye!");
+                break;
+            }
+        }
+    }
 
     Ok(())
 }
